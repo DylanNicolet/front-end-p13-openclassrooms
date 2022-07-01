88e3c08c0e72dabb4829b0bc9291200ee8d59c5a
DylanLaptop <dylannicolet@hotmail.com>
2022-07-01 16:56:31 +0400

    Implemented: Initial React-Router routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,22 +1,15 @@
-import "../sass/App.css"
+import "../sass/App.css";
+import { Outlet } from "react-router-dom";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <Outlet />
+      <Footer />
+    </main>
   );
 }
 
