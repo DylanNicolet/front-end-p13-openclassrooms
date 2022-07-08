c7e07fd3f620cb0b4f730dd95aa04bc12eb7ed25
DylanLaptop <dylannicolet@hotmail.com>
2022-07-08 12:09:32 +0400

    Fixed: Header content

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from "../images/argentBankLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { faUserCircle, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUserToken, updateUserName, updatePassword, updateFirstName, updateLastName } from "../redux/userDataSlice";
 
 /**
  * Display a header for the whole app
@@ -14,17 +16,43 @@ import { useNavigate } from "react-router-dom";
  * ) 
  */
 export default function Header(){
-    let navigate = useNavigate()
+    const firstName = useSelector((state) => state.userData.firstName)
+    const rememberMe = useSelector((state => state.userData.rememberMe))
+    const dispatch = useDispatch()
 
-    function handleClick(){
+    let navigate = useNavigate()
+    let location = useLocation()
+
+    function handleSignIn(){
         navigate("/login")
+    }
+
+    function handleSignOut(){
+        navigate("/")
+        dispatch(updateFirstName(""))
+        dispatch(updateLastName(""))
+        dispatch(updateUserToken(""))
+        if(!rememberMe){
+            dispatch(updateUserName(""))
+            dispatch(updatePassword(""))
+        }
     }
 
     return(
         <header>
             <img src={logo} alt="" />
-            <FontAwesomeIcon icon={faUserCircle} className="header__icon"/>
-            <button onClick={handleClick}>Sign In</button>
+            <section className={location.pathname !== "/profile" && "header__content"}>
+                <section className="icon-with-name">
+                    <FontAwesomeIcon icon={faUserCircle} className="header__icon"/>
+                    {location.pathname === "/profile" && <p>{firstName}</p>}
+                </section>
+                {location.pathname === "/profile"? 
+                    <section className="sign-out">
+                        <FontAwesomeIcon icon={faRightFromBracket}/>
+                        <button onClick={handleSignOut}>Sign Out</button>
+                    </section> : <button onClick={handleSignIn}>Sign In</button>
+                }
+            </section>
         </header>
     )
 }
